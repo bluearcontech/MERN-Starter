36cb41974ad221fac7b62c35bd9d9b37716513f7
Ionescu Bebe <blueskymobile@hotmail.com>
2017-08-10 23:23:48 +0800

    Add Features Page

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import CoreLatyout from '../layouts/CoreLayout'
 import Home from './Home'
+import Feature from './Feature'
 
 export default (
     <Route path="/" component={CoreLatyout}>
         <IndexRoute component={Home} />
+        <Route path="/features" component={Feature} />
     </Route>
 )
