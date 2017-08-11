f69c59590d32e9154c049b9f979315614b12cddf
Ionescu Bebe <blueskymobile@hotmail.com>
2017-08-11 22:19:08 +0800

    refactor store

--- app/store/location.js
+++
@@ -1,9 +0,0 @@
-export const LOCATION_CHANGE = 'LOCATION_CHANGE'
-
-const initialState = null
-
-export default (state = initialState, action) => (
-  action.type === LOCATION_CHANGE
-    ? action.payload
-    : state
-)
